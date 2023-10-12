6f39c3b94905b6aa6d202722a38dc6970aa60004
reliable99 <abdulmuhminsodiq@gmail.com>
2023-10-12 11:17:36 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+console.log("comments");
+
+//single line comments
+// this is asingle line comment
+
+/* 
+ This 
+ is 
+ a 
+ multi
+ line
+ comments
+ */
+
+// creating a variable using var keyword
+// var variableName = "test";
+
+// creating a variable using let keyword
+// let variableName = "test";
+
+// creating a variable using const keyword
+const variableName = "test";
+
+console.log("variableName");
